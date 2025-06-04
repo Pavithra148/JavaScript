159e065296d5bbcd47f220b7870b6f63d7f970b8
Pavithra148 <Pavithra.thangarasu14@gmail.com>
2025-06-04 17:06:40 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,17 @@
+
+let firstName = "Pavithra";          // String
+let companyName = "Cellarstone";     // String
+let mobileNumber = 9750383819;       // Number
+let isAutomation = true;             // Boolean
+let hasPlaywright;                   // Undefined (no value assigned)
+
+
+console.log("firstName:", firstName, "| Type:", typeof firstName);         // string
+console.log("companyName:", companyName, "| Type:", typeof companyName);   // string
+console.log("mobileNumber:", mobileNumber, "| Type:", typeof mobileNumber); // number
+console.log("isAutomation:", isAutomation, "| Type:", typeof isAutomation); // boolean
+console.log("hasPlaywrite:",hasPlaywright,"-",typeof hasPlaywright);
+
+
+
+
