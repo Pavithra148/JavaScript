a1a98a90ede28094d523c4e29b3402292abc3fac
Pavithra148 <Pavithra.thangarasu14@gmail.com>
2025-06-04 17:04:36 +0530

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,10 +1,1 @@
-let companyName1="Testleaf";
 
-let test=companyName1.split("");
-
-let reversed="";
-for(let i=test.length-1;i>=0;i--){
- reversed+=test[i].toLowerCase();
-}
-
-console.log(reversed);
